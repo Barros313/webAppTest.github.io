c815ab5c9ae5e986d047c112e4d008a64f43909b
Gabriel Barros <118929483+Barros313@users.noreply.github.com>
2024-03-14 08:48:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,6 +16,38 @@ let formValidation = () => {
         console.log("failure");
     } else {
         console.log("success");
-        msg.innerHTML = "Success";
+        msg.innerHTML = "";
+        acceptData();
     }
 };
+
+let data = {};
+
+let acceptData = () => {
+    data["text"] = input.value;
+    console.log(data);
+
+    createPost();
+};
+
+let createPost = () => {
+    posts.innerHTML += `
+    <div>
+        <p>${data.text}</p>
+            <span class="options"> 
+                <i onClick="editPost(this)" class="fas fa-edit"></i>
+                <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+            </span>
+    </div>
+    `;
+    input.value = "";
+}
+
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+}
+
+let editPost = (e) => {
+    input.value = e.parentElement.previousElementSibling.innerHTML;
+    deletePost(e);
+}
